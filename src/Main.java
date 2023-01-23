1618bae55396dcb698c47ae19536babb86810c4e
Johan <johan.isberg@outlook.com>
2023-01-23 15:48:19 +0100

    Ändrade string i filen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! Change 3");
+        System.out.println("Hello world!");
+        System.out.println("Testar Git och GitHub för första gången :)");
     }
 }
